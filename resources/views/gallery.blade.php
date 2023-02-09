c82cffe5a0f66afdaeeed59c34a4cbb48451a1ac
naufal abyan <naufalabyan972@gmail.com>
2023-02-09 08:57:17 +0700

    first commit

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <title>Data Gallery</title>
+    </head>
+<body>
+    <nav class="navbarnavbar-expand-sm navbar-dark bg-dark">
+        <div class="container">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="siswa">Data Siswa</a>
+                </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="/guru">Data Guru</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/gallery">Gallery</a> 
+                </li>       
+            </ul>
+        </div>
+    </nav>
+    <img src="ochi rosdiana 11.jpg">
+</body>
+</html>
